ee5b41e3955cccc6073568368f499f22f47e529e
Prakash <prakashsingha@gmail.com>
2026-05-16 22:10:32 +0800

    Set up logs and middleware

--- src/app.js
+++ src/app.js
@@ -1,11 +1,30 @@
 import express from 'express';
+import logger from '#config/logger.js';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
 
 const app = express();
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use(
+  morgan('combined', {
+    stream: {
+      write: message => logger.info(message.trim()),
+    },
+  })
+);
 
 // Middleware to parse JSON bodies
 app.use(express.json());
 
 app.get('/', (req, res) => {
+  logger.info('Hello from Acquisitions API');
   res.json({ message: 'Welcome to the Acquisitions API!' });
 });
 
