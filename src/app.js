68da515fa5a19105e63a0ace66b268a916564920
Prakash <prakashsingha@gmail.com>
2026-05-18 19:17:05 +0800

    Set up tests

--- src/app.js
+++ src/app.js
@@ -49,4 +49,9 @@ app.get('/api', (req, res) => {
 
 app.use('/api/auth', authRouter);
 app.use('/api/users', usersRoutes);
+
+app.use((req, res) => {
+  res.status(404).json({ error: 'Not Found', message: 'Route not found' });
+});
+
 export default app;
